77aa1fe64d513cbe6fec60c7be144da8211d198f
ZH46 <769050668@qq.com>
2020-12-22 22:07:39 +0800

    Merge branch 'main' of D:\work\Bidding with conflicts.

--- src/main/java/GetData.java
+++ src/main/java/GetData.java
@@ -1,7 +1,7 @@
-import site.CCGP_BeiJing;
+import site.CCGP_TianJin;
 
 public class GetData {
     public static void main(String[] args) {
-        new CCGP_BeiJing().start();
+        new CCGP_TianJin().start();
     }
 }
