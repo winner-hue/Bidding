5f0b8aa7014a3095890b4cf178b84823df8c2706
haoaremesda <1771380243@qq.com>
2021-03-04 17:55:19 +0800

    适配爬取数据Relu

--- src/main/java/site/CCGP_ShanDong.java
+++ src/main/java/site/CCGP_ShanDong.java
@@ -1,6 +1,7 @@
 package site;
 
 import com.alibaba.fastjson.JSONObject;
+import com.sun.tools.corba.se.idl.StringGen;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -21,21 +22,22 @@ import static util.Download.getHttpBody;
 
 public class CCGP_ShanDong extends WebGeneral {
     private static Logger logger = LoggerFactory.getLogger(CCGP_ShanDong.class);
-    private static String relu = "{'2500': {'authorRelu': 'div.info midea:eq(1)', 'priceRelu': 'tbody tr td:eq(3)', 'price_unit': '万元',\n" +
-            "               'addTimeRelu': 'div.info midea:eq(0)', 'addTimeParse': 'yyyy年MM月dd日 HH时mm分ss秒'},\n" +
-            "      '0301': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': 'tbody tr td:eq(4)', 'price_unit': '万元',\n" +
-            "               'addTimeRelu': 'div.info midea:eq(0)', 'addTimeParse': 'yyyy年MM月dd日 HH时mm分ss秒'},\n" +
-            "      '2102': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': 'td:containsOwn(预算金额)',\n" +
-            "               'addTimeRelu': 'div.info midea:eq(0)', 'addTimeParse': 'yyyy年MM月dd日 HH时mm分ss秒'},\n" +
-            "      '0305': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': '',\n" +
-            "               'addTimeRelu': 'div.info midea:eq(0)', 'addTimeParse': 'yyyy年MM月dd日 HH时mm分ss秒'},\n" +
-            "      '0302': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': 'tbody tr td:eq(4)', 'price_unit': '元',\n" +
-            "               'addTimeRelu': 'div.info midea:eq(0)', 'addTimeParse': 'yyyy年MM月dd日 HH时mm分ss秒'},\n" +
-            "      '0306': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': '',\n" +
-            "               'addTimeRelu': 'div.info midea:eq(0)', 'addTimeParse': 'yyyy年MM月dd日 HH时mm分ss秒'},\n" +
-            "      '2502': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': 'td:containsOwn(合同金额)',\n" +
-            "               'addTimeRelu': 'div.info midea:eq(0)', 'addTimeParse': 'yyyy年MM月dd日 HH时mm分ss秒'}\n" +
-            "          }";
+    private static String relu = "{\"province\": {'2500': {'authorRelu': 'div.info midea:eq(1)', 'priceRelu': 'tbody tr td:eq(3)', 'price_unit': '万元'},\n" +
+            "                   '0301': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': 'tbody tr td:eq(4)', 'price_unit': '万元'},\n" +
+            "                   '2102': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': 'td:containsOwn(预算金额)'},\n" +
+            "                   '0305': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': ''},\n" +
+            "                   '0302': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': 'tbody tr td:eq(4)', 'price_unit': '元'},\n" +
+            "                   '0306': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': ''},\n" +
+            "                   '2502': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': 'td:containsOwn(合同金额)'}\n" +
+            "                   },\n" +
+            "      \"city\": {'2504': {'authorRelu': 'h1.title', 'priceRelu': 'tbody tr td:eq(3)|td:containsOwn(预算金额)', 'price_unit': '万元'},\n" +
+            "               '0303': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': 'tbody tr td:eq(4)', 'price_unit': '万元'},\n" +
+            "               '2106': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': 'td:containsOwn(预算金额)', 'price_unit': '万元'},\n" +
+            "               '0305': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': ''},\n" +
+            "               '0304': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': 'tbody tr td:eq(4)', 'price_unit': '元'},\n" +
+            "               '0306': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': ''},\n" +
+            "               '2505': {'authorRelu': 'td:containsOwn(采购人)', 'priceRelu': 'td:containsOwn(合同金额)'}\n" +
+            "               }}";
     public static JSONObject relus = JSONObject.parseObject(relu);
 
     @Override
@@ -72,18 +74,16 @@ public class CCGP_ShanDong extends WebGeneral {
     @Override
     public void run() {
         setValue();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        String end_time = sdf.format((new Date()).getTime());
-        String start_time = Util.getLastMonth(null, 3);
         String url = "http://www.ccgp-shandong.gov.cn/sdgp2017/site/listnew.jsp";
-        String[] colcodes = {"2500", "0301", "2102", "0305", "0302", "0306", "2502"};
-//        String[] colcodes = {"0302", "0305"};
-        String[] urls = new String[colcodes.length];
-        int i = 0;
-        for (String colcode:colcodes) {
-            urls[i++] = url.concat("&#44subject=&pdate=".concat("&kindof=&unitname=&projectname=&projectcode=&colcode=".concat(colcode).concat("&curpage=1&grade=province&firstpage=1")));
+        final String[] colcodes = {"2500", "0301", "2102", "0305", "0302", "0306", "2502"}, citys = {"2504", "0303", "2106", "0305", "0304", "0306", "2505"};
+        Map<String, List<String>> test = new HashMap<String, List<String>>(){{put("province", Arrays.asList(colcodes));put("city", Arrays.asList(citys));}};
+        List<String> urls = new ArrayList<String>();
+        for (String key:test.keySet()) {
+            for (String colcode:test.get(key)) {
+                urls.add(url.concat("&#44subject=&pdate=".concat("&kindof=&unitname=&projectname=&projectcode=&colcode=".concat(colcode).concat("&curpage=1&grade=".concat(key).concat("&firstpage=1")))));
+            }
         }
-        this.main(urls);
+        this.main(urls.toArray(new String[urls.size()]));
         Bidding.cout.decrementAndGet();
     }
 
@@ -93,6 +93,8 @@ public class CCGP_ShanDong extends WebGeneral {
         nodeListRelu = "ul.news_list2 li";
         titleRelu = "h1.title";
         fullcontentRelu = "div#textarea";
+        addTimeRelu = "div.info midea:eq(0)";
+        addTimeParse = "yyyy年MM月dd日 HH时mm分ss秒";
     }
 
     @Override
@@ -164,7 +166,7 @@ public class CCGP_ShanDong extends WebGeneral {
         }
     }
 
-    protected String getDetail(Document parse, String colcode) {
+    protected String getDetail(Document parse) {
         try {
             return parse.select(fullcontentRelu).get(0).text();
         } catch (Exception e) {
@@ -176,10 +178,13 @@ public class CCGP_ShanDong extends WebGeneral {
     protected void extract(Document parse, StructData data, String pageSource) {
         logger.info("==================================");
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        String start_str = "./listnew.jsp?colcode=";
+        String start_str = "./listnew.jsp?colcode=", grade_str = "grade=";
         int begin_index = pageSource.indexOf(start_str) + start_str.length();
         String colcode = pageSource.substring(begin_index, begin_index + 4);
-        Date date = getAddTime(parse, colcode);
+        begin_index = pageSource.indexOf(grade_str) + grade_str.length();
+        String grade = pageSource.substring(begin_index, begin_index + 4);
+        grade = relus.containsKey(grade)? grade: (String) relus.keySet().toArray()[0];
+        Date date = getAddTime(parse);
         data.setAdd_time(date.getTime());
         String title = getTitle(parse);
         logger.info("title: " + title);
@@ -193,13 +198,13 @@ public class CCGP_ShanDong extends WebGeneral {
         int cityId = cityIdRelu;
         logger.info("cityId: " + cityId);
         data.setCity_id(cityId);
-        String author = getAuthor(parse, colcode);
+        String author = getAuthor(parse, colcode, grade);
         logger.info("author: " + author);
         data.setAuthor(author);
-        String price = getPrice(parse, colcode);
+        String price = getPrice(parse, colcode, grade);
         logger.info("price: " + price);
         data.setPrice(price);
-        String detail = getDetail(parse, colcode);
+        String detail = getDetail(parse);
         logger.info("detail: " + detail);
         data.setFullcontent(detail);
         String annex = getAnnex(parse);
@@ -209,35 +214,54 @@ public class CCGP_ShanDong extends WebGeneral {
         data.setAdd_time_name(add_time_name);
     }
 
-    protected String getPrice(Document parse, String colcode) {
+    protected String getPrice(Document parse, String colcode, String grade) {
         try {
             double price = 0;
-            String price_str = null;
-            Elements els = parse.select(relus.getJSONObject(colcode).get("priceRelu").toString());
-            if (els.size() <= 1) {
-                price_str = els.get(0).text();
-            }else if (els.size() == 2) {
-                price_str = els.get(1).text();
-            } else {
-                for (int i = 1; i < els.size(); i++) {
-                    price = price + Double.parseDouble(els.get(i).text().replaceAll("/", ""));
+            String price_str = null, break_where = "no";
+            for (String price_relu:relus.getJSONObject(grade).getJSONObject(colcode).get("priceRelu").toString().split("\\|")) {
+                Elements els = parse.select(price_relu);
+                switch (els.size()){
+                    case 0 :
+                        continue;
+                    case 1 :
+                        price_str = els.get(0).text();
+                        break_where = "yes";
+                        break;
+                    case 2 :
+                        price_str = els.get(1).text();
+                        break_where = "yes";
+                        break;
+                    default :
+                        for (int i = 1; i < els.size(); i++) {
+                            price = price + Double.parseDouble(els.get(i).text().replaceAll("/", ""));
+                        }
+                        price_str = String.valueOf(price);
                 }
-                price_str = String.valueOf(price);
+                if (break_where.equals("yes")) {
+                    break;
+                }
             }
-            price_str = price_str.indexOf("万元") != -1 || price_str.indexOf("元") != -1? price_str: price_str + relus.getJSONObject(colcode).get("price_unit").toString();
-            price_str = price_str.indexOf("：") !=-1? price_str.split("：")[1]: price_str;
+            price_str = price_str.indexOf("万元") != -1 || price_str.indexOf("元") != -1? price_str: price_str + relus.getJSONObject(grade).getJSONObject(colcode).get("price_unit").toString();
+            if (price_str.indexOf("：") !=-1){
+                String[] arrs = price_str.split("：");
+                price_str = arrs[arrs.length - 1];
+            }
             return price_str;
         } catch (Exception e) {
             return "";
         }
     }
 
-    protected String getAuthor(Document parse, String colcode) {
+    protected String getAuthor(Document parse, String colcode, String grade) {
         try {
-            String author = parse.select(relus.getJSONObject(colcode).get("authorRelu").toString()).get(0).text();
+            Calendar date = Calendar.getInstance();
+            String year = String.valueOf(date.get(Calendar.YEAR));
+            String author = parse.select(relus.getJSONObject(grade).getJSONObject(colcode).get("authorRelu").toString()).get(0).text();
             author = author.indexOf("发布人") != -1? author.split("：")[1]: author;
             author = author.indexOf("采购人") != -1? author.split("：")[1]: author;
             author = author.indexOf("地址") != -1? author.split(" ")[0]: author;
+            author = author.indexOf(year) != -1? author.split(year)[0]: author;
+            author = parse.text().indexOf("采购单位") != -1? parse.select("td:containsOwn(采购单位)").get(0).text().split("：")[1]: author;
             return author;
         } catch (Exception e) {
             return "";
@@ -255,14 +279,14 @@ public class CCGP_ShanDong extends WebGeneral {
         return "";
     }
 
-    protected Date getAddTime(Element element, String colcode) {
+    protected Date getAddTime(Element element) {
         Date parse = null;
         try {
-            String addTime = element.select(relus.getJSONObject(colcode).get("addTimeRelu").toString()).get(0).text();
+            String addTime = element.select(this.addTimeRelu).get(0).text();
             if (addTime.indexOf("发布时间") != -1) {
                 addTime = addTime.split("：")[1];
             }
-            SimpleDateFormat format = new SimpleDateFormat(relus.getJSONObject(colcode).get("addTimeParse").toString());
+            SimpleDateFormat format = new SimpleDateFormat(this.addTimeParse);
             parse = format.parse(addTime);
         } catch (Exception e) {
             logger.error("获取日期错误：" + e, e);
@@ -284,7 +308,7 @@ public class CCGP_ShanDong extends WebGeneral {
                 resultData.setArticleurl(url);
                 // 获取发布时间
                 String hits = element.select("span.hits").get(0).text();
-                SimpleDateFormat format = new SimpleDateFormat(this.addTimeParse);
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
                 Date date = format.parse(hits);
                 long addTime = date.getTime();
                 logger.info("addTime: " + addTime);
