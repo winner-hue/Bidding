bfcd6b85f997b6a37e9ffcbe1cb0bd9864878815
wangpengjie <1344246287@qq.com>
2021-05-03 09:24:30 +0800

    修改湖北站点

--- src/main/java/GetData.java
+++ src/main/java/GetData.java
@@ -9,8 +9,8 @@ public class GetData {
 //        new CCGP_BeiJing().start();
 //        new CCGP_TianJin().start();
 //        new CCGP_HeBei().start();
-        new CCGP_HuBei().start();
-//        new CCGP_HuNan().start();
+//        new CCGP_HuBei().start();
+        new CCGP_HuNan().start();
 //        new CCGP_ShangXi().start();
 //        new CCGP_LiaoNing().start();
 //        new CCGP_HaiNan().start();
